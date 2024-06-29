9a5bdd8c5fa0b67527c8be5c31b903ac2c3180f6
Shubham Kumar <kshubhamsingh96311@gmail.com>
2024-06-29 17:40:47 +0530

    Update actionTypes.js

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -7,3 +7,4 @@ export const FILTER_TODOS = 'FILTER_TODOS';
 export const MARK_ALL_COMPLETED = 'MARK_ALL_COMPLETED';
 export const UPDATE_SEARCH_TERM = 'UPDATE_SEARCH_TERM';
 export const SET_TODOS = 'SET_TODOS'; 
+export const EDIT_TODO = 'EDIT_TODO';
